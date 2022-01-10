1227bc08830557a63face48cc58137a8b2b2ef19
Tiffany Wise <TWise@figure.com>
2022-01-09 20:43:01 -0500

    move Sudoku class to separate file

--- src/Board.js
+++ src/Board.js
@@ -106,25 +106,4 @@ function Board(props) {
     )
 }
 
-class Sudoku extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            board: Array(9).fill(Array(9).fill(null)),
-        }
-    }
-
-    handleClick(i, j) {
-        console.log(`i = ${i}, j = ${j}`);
-        console.log(this.state.board);
-    }
-
-    render() {
-        return (
-            <Board size={3} onClick={(i, j) => this.handleClick(i, j)} board={this.state.board}/>
-        )
-    }
-}
-
-
-export default Sudoku;
+export default Board;
