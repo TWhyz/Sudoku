273d29f92dc94d67a2dd14bff71bd15bdd11af10
Tiffany Wise <TWise@figure.com>
2022-01-16 11:24:56 -0500

    Added function to highlight the entire row and column when hovering over a square in the board.

--- src/Sudoku.js
+++ src/Sudoku.js
@@ -6,18 +6,51 @@ class Sudoku extends React.Component {
         super(props);
         this.state = {
             board: Array(9).fill(Array(9).fill(null)),
+            highlightedSquares: Array(9).fill(null),
         }
     }
 
-    handleClick(i, j) {
-        console.log(`i = ${i}, j = ${j}`);
+    handleClick(row, col) {
+        console.log(`i = ${row}, j = ${col}`);
         console.log(this.state.board);
     }
 
+    highlightRowAndColumn(row, col) {
+        let highlighted = this.state.highlightedSquares.slice();
+
+        for (let r = 0; r < highlighted.length; r++) {
+            highlighted[r] = Array(9).fill(null);
+
+            for (let c = 0; c < highlighted[r].length; c++) {
+                if (highlighted[row]) highlighted[row][c] = 'hover';
+                highlighted[r][col] = 'hover';
+            }
+        }
+
+        this.setState({
+            highlightedSquares: highlighted,
+        });
+    }
+
+    removeHighlight(row, col) {
+        this.setState({
+            highlightedSquares: Array(9).fill(null),
+        });
+    }
+
+
     render() {
+        const size = 3;
         return (
-            <Board size={3} onClick={(i, j) => this.handleClick(i, j)} board={this.state.board}/>
-        )
+            <Board
+                size={size}
+                onClick={(row, col) => this.handleClick(row, col)}
+                highlight={(row, col) => this.highlightRowAndColumn(row, col)}
+                removeHighlight={(row, col) => this.removeHighlight(row, col)}
+                board={this.state.board}
+                squareClassname={this.state.highlightedSquares}
+            />
+        );
     }
 }
 
